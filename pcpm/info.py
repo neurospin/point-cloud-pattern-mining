63a831386752576f02428d1156d3361296bf79b9
Marco Pascucci <marpas.paris@gmail.com>
2022-02-01 15:00:49 +0100

    increment version

--- pcpm/info.py
+++ pcpm/info.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 version_major = 0
-version_minor = 0
-version_micro = 40
+version_minor = 1
+version_micro = 0
 version_extra = ''
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
